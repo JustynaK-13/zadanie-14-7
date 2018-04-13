b4cbc2614617085f9b0eb6de2eba68a0987837b9
Justyna Kaczorowska <js.kaczorowska@gmail.com>
2018-04-13 13:43:38 +0200

    Add modified content

--- components/Contact.js
+++ components/Contact.js
@@ -3,19 +3,20 @@ var Contact = React.createClass({
     item: React.PropTypes.object.isRequired,
   },
 
-  render: function() {
+render: function() {
     return (
-      React.createElement('div', {className: 'contactItem'},
-        React.createElement('img', {
-          className: 'contactImage',
-          src: 'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'
-        }),
-        React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
-        React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
-        React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
-          this.props.item.email
-        )
-      )
+    <div className={'contactItem'}>
+      <img className={'contactImage'} src={'link-do-obrazka.png'}/>
+      <p className={'contactLabel'}>
+          Imię: {this.props.contact.firstName}
+      </p>
+      <p className={'contactLabel'}>
+          Nazwisko: {this.props.contact.lastName}
+      </p>
+      <a href={'mailto:' + this.props.item.email}>
+          {this.props.item.email}
+      </a>
+    </div>
     )
   },
-});
+)};    
